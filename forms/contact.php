5a40f3fe1ac1b417072cf0374a6cc5cb64cf172d
eswaran04 <81405897+eswaran04@users.noreply.github.com>
2022-08-02 11:56:35 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -11,21 +11,21 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+  /*
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
+  */
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'kpeswaran04@gmail.com',
+    'password' => 'rcvlwkalhiaxofju',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
